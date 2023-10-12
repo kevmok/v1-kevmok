4bd31ebc1e25cc370cc0342cc73172c9b1da4f99
Kevin Mok <mok.kevin11@gmail.com>
2023-10-11 23:39:25 -0500

    Add experience

--- src/components/ui/heading.tsx
+++ src/components/ui/heading.tsx
@@ -9,7 +9,7 @@ const Heading: React.FC<HeadingProps> = ({ children, title }) => {
 			id={title}
 			className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
 		>
-			<div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 md:-mx-12 md:px-12">
+			<div className="top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 md:-mx-12 md:px-12">
 				<h2 className="text-sm font-semibold uppercase tracking-widest text-slate-200">
 					{title.toUpperCase()}
 				</h2>
