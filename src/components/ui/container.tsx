01ac0a7bffb7d5e82445c0871cef1373dd731c6b
Kevin Mok <mok.kevin11@gmail.com>
2023-10-16 16:16:42 -0500

    Fix x overflow

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -4,7 +4,7 @@ interface ContainerProps {
 
 const Container: React.FC<ContainerProps> = ({ children }) => {
 	return (
-		<div className="px-6 py-12 mx-auto w-full max-w-6xl md:px-12 md:py-20 lg:px-24">
+		<div className="px-6 py-12 mx-auto w-full max-w-6xl md:px-12 md:py-20 lg:px-24 overflow-x-hidden">
 			{children}
 		</div>
 	);
