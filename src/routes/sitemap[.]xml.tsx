577f6d894976bfedc34f197443e948161cb2ce35
Kevin Mok <mok.kevin11@gmail.com>
2026-02-10 09:54:32 -0600

    fix: serve sitemap.xml as proper XML with correct content-type

--- src/routes/sitemap[.]xml.tsx
+++ src/routes/sitemap[.]xml.tsx
@@ -4,30 +4,29 @@ import { allPosts } from 'content-collections';
 const SITE_URL = 'https://kevmok.com';
 
 export const Route = createFileRoute('/sitemap.xml')({
-	loader: async () => {
-		const routes = [
-			{ url: `${SITE_URL}/`, lastModified: new Date().toISOString() },
-			{ url: `${SITE_URL}/projects`, lastModified: new Date().toISOString() },
-		];
+	server: {
+		handlers: {
+			GET: async () => {
+				const routes = [
+					{ url: `${SITE_URL}/`, lastModified: new Date().toISOString() },
+					{
+						url: `${SITE_URL}/projects`,
+						lastModified: new Date().toISOString(),
+					},
+				];
 
-		const posts = allPosts
-			.filter(post => !post.draft)
-			.map(post => ({
-				url: `${SITE_URL}/n/${post._meta.path}`,
-				lastModified: new Date(post.date).toISOString(),
-			}));
+				const posts = allPosts
+					.filter(post => !post.draft)
+					.map(post => ({
+						url: `${SITE_URL}/n/${post._meta.path}`,
+						lastModified: new Date(post.date).toISOString(),
+					}));
 
-		return { routes: [...routes, ...posts] };
-	},
-	component: SitemapComponent,
-});
+				const allRoutes = [...routes, ...posts];
 
-function SitemapComponent() {
-	const { routes } = Route.useLoaderData();
-
-	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+				const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${routes
+${allRoutes
 	.map(
 		route => `	<url>
 		<loc>${route.url}</loc>
@@ -37,5 +36,12 @@ ${routes
 	.join('\n')}
 </urlset>`;
 
-	return <pre className="whitespace-pre-wrap font-mono text-sm">{sitemap}</pre>;
-}
+				return new Response(sitemap, {
+					headers: {
+						'Content-Type': 'application/xml',
+					},
+				});
+			},
+		},
+	},
+});
